b2e5949e3b23ecc70abcc78ec1f88e972556a7b4
vasaka <vasaka@gmail.com>
2015-03-19 19:35:41 +0700

    Create install_test_app.sh

---
+++ elliptics/install_test_app.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+tar -cf test_app.tar test_app.py
+cocaine-tool app upload --manifest test_app_manifest.json --package test_app.tar --name test_app
+cocaine-tool profile upload --profile process_isolate.profile --name test_app
+dnet_ioclient -r localhost:1025:2 -g 1 -c "test_app@start-task"
