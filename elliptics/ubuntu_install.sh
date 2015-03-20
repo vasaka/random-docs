48d0959917d195ca19d5d03fa5c0eb29e7b48002
vasaka <vasaka@gmail.com>
2015-03-20 16:36:08 +0700

    Create ubuntu_install.sh

---
+++ elliptics/ubuntu_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update
+apt-get install curl
+
+echo >> /etc/apt/sources.list
+echo "deb http://repo.reverbrain.com/trusty/ current/amd64/" >> /etc/apt/sources.list
+echo "deb http://repo.reverbrain.com/trusty/ current/all/" >> /etc/apt/sources.list
+
+curl http://repo.reverbrain.com/REVERBRAIN.GPG | apt-key add -
+
+apt-get update
+apt-get install elliptics -y
