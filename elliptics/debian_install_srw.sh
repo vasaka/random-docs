f3ae501888215bb712b24cb4ed0ad3f32aa67519
vasaka <vasaka@gmail.com>
2015-03-24 20:13:15 +0700

    Update debian_install_srw.sh

--- elliptics/debian_install_srw.sh
+++ elliptics/debian_install_srw.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl http://ftp.cz.debian.org/debian/pool/main/p/python-opster/python-opster_4.1-1_all.deb -o python-opster_4.1-1_all.deb
 curl http://ftp.cz.debian.org/debian/pool/main/p/python-tornado/python-tornado_3.2.2-1.1_amd64.deb -o python-tornado_3.2.2-1.1_amd64.deb
 dpkg -i python-opster_4.1-1_all.deb python-tornado_3.2.2-1.1_amd64.deb
