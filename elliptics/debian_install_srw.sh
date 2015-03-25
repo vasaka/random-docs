e556f6c5fa918837055a27cfb91fe18b7dd6cee0
vasaka <vasaka@gmail.com>
2015-03-25 16:00:15 +0700

    Update debian_install_srw.sh

--- elliptics/debian_install_srw.sh
+++ elliptics/debian_install_srw.sh
@@ -4,3 +4,6 @@ curl http://ftp.cz.debian.org/debian/pool/main/p/python-opster/python-opster_4.1
 curl http://ftp.cz.debian.org/debian/pool/main/p/python-tornado/python-tornado_3.2.2-1.1_amd64.deb -o python-tornado_3.2.2-1.1_amd64.deb
 dpkg -i python-opster_4.1-1_all.deb python-tornado_3.2.2-1.1_amd64.deb
 apt-get -f install -y
+
+apt-get install python-pip -y
+pip install backports.ssl_match_hostname
