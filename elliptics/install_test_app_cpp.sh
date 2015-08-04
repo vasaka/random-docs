81b2727dc9f3bbfaec0c92d683ca01f9e8c2a06e
vasaka <vasaka@gmail.com>
2015-08-04 12:43:10 +0700

    Update install_test_app_cpp.sh

--- elliptics/install_test_app_cpp.sh
+++ elliptics/install_test_app_cpp.sh
@@ -3,5 +3,6 @@
 tar -cf test_app_cpp.tar test_app_cpp/build/test_app_cpp
 cocaine-tool app upload --manifest test_app_manifest_cpp.json --package test_app_cpp.tar --name test_app_cpp
 cocaine-tool profile upload --profile process_isolate.profile --name test_app_cpp
+dnet_ioclient -r localhost:1025:2 -g 1 -c "test_app_cpp@stop-task"
 dnet_ioclient -r localhost:1025:2 -g 1 -c "test_app_cpp@start-task"
 dnet_ioclient -r localhost:1025:2 -g 1 -c "test_app_cpp@init"
