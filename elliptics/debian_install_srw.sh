e66a21d64b26d2a9141799bb8e785b025f2ce929
vasaka <vasaka@coccoc.com>
2015-09-10 15:53:59 +0700

    use wget instead of curl.

--- elliptics/debian_install_srw.sh
+++ elliptics/debian_install_srw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl http://ftp.cz.debian.org/debian/pool/main/p/python-opster/python-opster_4.1-1_all.deb -o python-opster_4.1-1_all.deb
-curl http://ftp.cz.debian.org/debian/pool/main/p/python-tornado/python-tornado_3.2.2-1.1_amd64.deb -o python-tornado_3.2.2-1.1_amd64.deb
+wget http://ftp.cz.debian.org/debian/pool/main/p/python-opster/python-opster_4.1-1_all.deb
+wget http://ftp.cz.debian.org/debian/pool/main/p/python-tornado/python-tornado_3.2.2-1.1_amd64.deb
 dpkg -i python-opster_4.1-1_all.deb python-tornado_3.2.2-1.1_amd64.deb
 apt-get -f install -y
 
